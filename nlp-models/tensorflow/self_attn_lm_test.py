806c1437ecabeb8dfa173f4d87a425362d6f3e0c
Zhedong Zheng <zhedongzheng@outlook.com>
2018-04-06 13:48:34 +0100

    notebooks

--- nlp-models/tensorflow/self_attn_lm_test.py
+++
@@ -1,9 +0,0 @@
-from self_attn_lm import LM
-
-
-if __name__ == '__main__':
-    with open('./temp/anna.txt') as f:
-        text = f.read()
-    
-    model = LM(text, seq_len=200)
-    log = model.fit()
